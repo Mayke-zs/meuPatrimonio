655507b4ca7775cd0651777c4664c4f7008b531f
MAYKE GONÇALVES PEREIRA <mayke_pereira@estudante.sesisenai.org.br>
2026-08-13 21:42:53 -0300

    primeiro commit

---
+++ src/main/java/br/com/senai/patrimonio/PatrimonioApplication.java
@@ -0,0 +1,25 @@
+package br.com.senai.patrimonio;
+
+import br.com.senai.patrimonio.model.Empresa;
+import br.com.senai.patrimonio.model.Endereco;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatrimonioApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(PatrimonioApplication.class, args);
+
+		Empresa empresa = new Empresa();
+		empresa.setRazaoSocial("Senai LTDA");
+		System.out.println(empresa.getRazaoSocial());
+
+		Endereco endereco = new Endereco();
+		endereco.setRua("Rod Josephina Lodette Vassoler");
+		System.out.println();
+		System.out.println(endereco.getRua());
+	}
+
+}
